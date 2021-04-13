459809c1ca96e883dade77046210ccbef3ee0330
dicetzy <82380701+dicetzy@users.noreply.github.com>
2021-04-13 12:55:29 +0800

    Update botConfig.js

--- botConfig.js
+++ botConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
-    token: "YOUR_TOKEN",
-    prefix: "PREFIX",
+    token: "ODMwNzY5MjcyODM3NjM2MTM2.YHLgLQ.tnRUlYx5jFVajPt-Wv_jpU6aRMM",
+    prefix: "sr.",
     admins: [
         "PEOPLE WHO CAN USE ADD MONEY (IDS)"
 ],
